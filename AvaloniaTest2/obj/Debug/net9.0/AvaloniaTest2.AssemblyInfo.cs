41d774cc29d19d5af3c0d528743d2d361edfa605
devpefer <devpefer@gmail.com>
2025-09-26 05:12:44 +0200

    Añadido simpletheme

--- AvaloniaTest2/obj/Debug/net9.0/AvaloniaTest2.AssemblyInfo.cs
+++ AvaloniaTest2/obj/Debug/net9.0/AvaloniaTest2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaloniaTest2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fe4a6546661c5900b5cb1325a044f0a20a687a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9554d1ba4ba310752510fbef0216f32df37f7dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaloniaTest2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaloniaTest2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
